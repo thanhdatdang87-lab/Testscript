36d76f17aba2050a83131512e3ea54d65ce9710a
thanhdatdang87-lab <thanhdatdang87@gmail.com>
2026-06-11 09:09:09 +0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,11 @@
+return {
+    Preset = "High",
+    Steps = {
+        { Name = "ConstantArray" },
+        { Name = "WrapInFunction" },
+        { Name = "ProxifyLocals" },
+        { Name = "EncryptStrings" },
+        { Name = "NumbersToExpressions" },
+        { Name = "Vmify" },
+    }
+}
